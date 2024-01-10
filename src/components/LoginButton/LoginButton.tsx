36a9e0172cad0301da193afa24a0bdf057630499
rigoarpi <rigoarpad1@gmail.com>
2024-01-10 18:54:54 +0100

    fix login, logout

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -2,12 +2,38 @@ import { useEffect } from "react";
 import { useClientStore } from "../../store/client.store";
 import Button from "../Button/Button";
 import { msalInstance, loginRequest } from "../../authConfig";
-import { useIsAuthenticated } from "@azure/msal-react";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
 
 export const LoginButton = () => {
   const isAuthenticated = useIsAuthenticated();
 
   const { setToken, token } = useClientStore();
+  const { instance, accounts, inProgress } = useMsal();
+  const account = accounts[0];
+
+  useEffect(() => {
+    const getToken = async () => {
+      if (!account || inProgress !== "none") {
+        return;
+      }
+
+      try {
+        const silentToken = await instance.acquireTokenSilent({
+          scopes: ["openid", "profile", "User.Read"],
+          account: account,
+        });
+
+        setToken(silentToken.accessToken);
+      } catch (error) {
+        // Handle error, possibly acquire token using a popup if silent fails
+        console.error(error);
+      }
+    };
+
+    if (isAuthenticated) {
+      getToken();
+    }
+  }, [isAuthenticated, account, inProgress, instance, setToken]);
 
   const login = async () => {
     try {
@@ -19,16 +45,16 @@ export const LoginButton = () => {
     }
   };
 
-  useEffect(() => {
-    // check for the account and set active account
-    const accounts = msalInstance.getAllAccounts();
-    if (accounts && accounts.length > 0 && isAuthenticated) {
-      msalInstance.setActiveAccount(accounts[0]);
-    }
-  }, []);
-
   const logout = () => {
     setToken("");
+
+    instance
+      .logoutPopup({
+        postLogoutRedirectUri: "/",
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   };
 
   return token && isAuthenticated ? (
