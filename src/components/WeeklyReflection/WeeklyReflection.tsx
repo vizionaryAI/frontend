e8b6a52679888bb302209941445460e4818ae3cf
rigoarpi <rigoarpad1@gmail.com>
2024-02-23 20:30:15 +0100

    add a condition

--- src/components/WeeklyReflection/WeeklyReflection.tsx
+++ src/components/WeeklyReflection/WeeklyReflection.tsx
@@ -1,13 +1,17 @@
+import { useClientStore } from "../../store/client.store";
 import { ChatBot } from "../ChatBot/ChatBot";
 import { UserWarning } from "../UserWarning/UserWarning";
 import * as S from "./WeeklyReflection.styles";
 
 export const WeeklyReflection = () => {
+  const { user } = useClientStore();
   return (
     <>
       <S.Container>
         <S.Title>Weekly Reflection</S.Title>
-        <UserWarning message="All messages sent can be seen by your organization's administrator. Please do not put any sensitive information like passwords into the chatbox!" />
+        {user.content_monitored_warning && (
+          <UserWarning message="All messages sent can be seen by your organization's administrator. Please do not put any sensitive information like passwords into the chatbox!" />
+        )}
       </S.Container>
       <ChatBot conversationType="weekly" />
     </>
