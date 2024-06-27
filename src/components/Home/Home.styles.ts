6a5554065ed618c5873da6f1ad3d3610388cddf3
rigoarpi <rigoarpad1@gmail.com>
2024-06-27 18:57:12 +0200

    modify home screen card

--- src/components/Home/Home.styles.ts
+++ src/components/Home/Home.styles.ts
@@ -3,46 +3,87 @@ import styled, { DefaultTheme } from "styled-components";
 export const Container = styled.div`
   display: flex;
   justify-content: space-around;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
 `;
 
-export const Card = styled.div<{ theme: DefaultTheme; disabled?: boolean }>`
+export const Card = styled.div<{
+  theme: DefaultTheme;
+  disabled?: boolean;
+  blurred?: boolean;
+}>`
   background-color: ${({ theme, disabled }) =>
     disabled ? theme.homeDisabledCardBackground : theme.homeCardBackground};
   color: ${({ theme }) => theme.textLight};
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   border-radius: 0.5rem;
   box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.775);
-  padding: 1.25rem;
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
   margin: 0.625rem;
   width: 15rem;
   height: 15rem;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   transition: box-shadow 0.2s ease-in-out;
-  opacity: ${({ disabled }) => (disabled ? 0.7 : 1)};
+  opacity: ${({ disabled }) => (disabled ? 0.85 : 1)};
+  position: relative;
+  overflow: hidden;
 
   &:hover {
     box-shadow: 0 0.5rem 1rem ${({ theme }) => theme.cardShadow};
   }
 
   h2 {
-    margin: 0;
-    padding: 0;
-    font-size: 1.5rem;
-    color: #333;
+    font-weight: normal;
+    font-size: 1.1rem;
+    color: ${({ theme }) => theme.textLight};
+    text-align: left;
   }
+
   @media (max-width: 768px) {
-    width: 10rem;
-    height: 10rem;
+    width: 15rem;
+    height: 15rem;
     padding: 0.4rem;
     margin: 0.4rem;
   }
 `;
 
-export const Notes = styled.p`
-  margin-top: 1rem;
-  font-size: 0.8rem;
-  color: gray;
+export const Notes = styled.p<{ theme: DefaultTheme }>`
+  font-size: 0.7rem;
+  color: ${({ theme }) => theme.textLight};
+  text-align: left;
+  font-weight: normal;
+  line-height: 1rem;
+`;
+
+export const Duration = styled.p<{ theme: DefaultTheme }>`
+  font-size: 0.9rem;
+  color: ${({ theme }) => theme.textLight};
+  text-align: right;
+  font-weight: normal;
+`;
+
+export const Overlay = styled.div<{ theme: DefaultTheme }>`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  backdrop-filter: blur(2px);
+
+  h2 {
+    color: ${({ theme }) => theme.textLight};
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
 `;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -22,16 +22,37 @@ export const Home = () => {
       {user.introduction_completed ? (
         <S.Container>
           <S.Card onClick={() => navigate("/daily-reflection")}>
-            Daily Reflection
+            <h2>Short Session</h2>
+            <S.Notes>
+              This is a focused and concise coaching session, designed
+              specifically for you to work intensively on a particular skill or
+              attribute you wish to improve.
+            </S.Notes>
+            <S.Duration>Duration 5-10mins</S.Duration>
           </S.Card>
-
           <S.Card
             disabled={!user.allow_weekly}
             onClick={() => user.allow_weekly && navigate("/weekly-reflection")}
           >
-            Weekly Reflection
-            {!user.weekly_open && <S.Notes>Open on Friday</S.Notes>}
-            {user.weekly_completed && <S.Notes>Already completed</S.Notes>}
+            <h2>Weekly Session</h2>
+            <S.Notes>
+              In these sessions, we'll take a step back to review your progress
+              over the past week. We’ll assess how well you're advancing towards
+              the goals we've set in previous sessions. Additionally, we’ll use
+              this time to continue developing the skills and attributes you're
+              most interested in improving.
+            </S.Notes>
+            <S.Duration> Duration 15-20mins</S.Duration>
+            {!user.weekly_open && (
+              <S.Overlay>
+                <h2>Open on Friday</h2>
+              </S.Overlay>
+            )}
+            {user.weekly_completed && (
+              <S.Overlay>
+                <h2>Already completed</h2>
+              </S.Overlay>
+            )}
           </S.Card>
         </S.Container>
       ) : (
